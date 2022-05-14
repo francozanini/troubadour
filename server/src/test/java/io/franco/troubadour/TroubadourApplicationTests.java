dd0679e036e322daaace4983410b3377ec03d41f
francozanini <francozanini1995@gmail.com>
2022-05-14 01:22:17 -0300

    initial commit

---
+++ server/src/test/java/io/franco/troubadour/TroubadourApplicationTests.java
@@ -0,0 +1,13 @@
+package io.franco.troubadour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubadourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
