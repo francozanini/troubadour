a06289e3452e29545d89ead736bb47bf2026bf28
francozanini <francozanini1995@gmail.com>
2022-05-14 22:30:47 -0300

    Song Test

---
+++ server/src/test/java/io/franco/troubadour/streaming/Song.java
@@ -0,0 +1,32 @@
+package io.franco.troubadour.streaming;
+
+public class Song {
+
+    private final String name;
+    private boolean isPlaying;
+
+    public Song(String name) {
+        this.name = name;
+        this.isPlaying = false;
+    }
+
+    public static Song named(String songName) {
+        return new Song(songName);
+    }
+
+    public boolean hasName(String nameToCompare) {
+        return this.name.equalsIgnoreCase(nameToCompare);
+    }
+
+    public void play() {
+        isPlaying = true;
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public void pause() {
+        isPlaying = false;
+    }
+}
---
+++ server/src/test/java/io/franco/troubadour/streaming/SongTest.java
@@ -0,0 +1,46 @@
+package io.franco.troubadour.streaming;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SongTest {
+
+    private static final String songName = "songName";
+
+    @Test
+    public void songCreatesWithName() {
+        Song song = Song.named(songName);
+        assertThat(song.hasName(songName.toUpperCase())).isTrue();
+    }
+
+    @Test
+    public void newSongIsNotPlaying() {
+        Song song = Song.named(songName);
+        assertThat(song.isPlaying()).isFalse();
+    }
+
+    @Test
+    public void songComparesNameLowerCase() {
+        Song song = Song.named(songName);
+        assertThat(song.hasName("name that isn't")).isFalse();
+    }
+
+    @Test
+    public void songCanBePlayed() {
+        Song song = Song.named(songName);
+        song.play();
+        assertThat(song.isPlaying()).isTrue();
+    }
+
+    @Test
+    public void songCanBePaused() {
+        Song song = Song.named(songName);
+        song.play();
+        song.pause();
+        assertThat(song.isPlaying()).isFalse();
+    }
+
+
+
+}
