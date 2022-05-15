ea4f8b755e8ff5737a95e531b31b8e1fc724a2e9
francozanini <francozanini1995@gmail.com>
2022-05-14 23:31:18 -0300

    GivenPlayCommand_WhenSongIsPlaying_ItSwitchesToNewSong, canPlaySongsFromAnywhereInThePlaylist, canPlaySongsFromTheBeginningOfPlaylist, canNotAddSameSongTwice

--- server/src/test/java/io/franco/troubadour/streaming/Playlist.java
+++ server/src/test/java/io/franco/troubadour/streaming/Playlist.java
@@ -5,6 +5,10 @@ import java.util.Set;
 
 public class Playlist {
 
+    public static final String ALREADY_CONTAINS_SONG = "Song is already in the playlist";
+    public static final String PLAYLIST_IS_EMPTY = "PLAYLIST_IS_EMPTY";
+    private static final String NO_SONG_PLAYING = "No song is being played";
+    private static final String SONG_NOT_FOUND = "Song is not on the playlist";
     private String name;
     private final Set<Song> songs = new HashSet<>();
 
@@ -21,6 +25,10 @@ public class Playlist {
     }
 
     public void addSong(String songNameToAdd) {
+        if (hasSong(songNameToAdd)) {
+            throw new RuntimeException(ALREADY_CONTAINS_SONG);
+        }
+
         songs.add(new Song(songNameToAdd));
     }
 
@@ -30,5 +38,41 @@ public class Playlist {
 
     public void removeSong(String songNameToRemove) {
         this.songs.removeIf(song -> song.isNamed(songNameToRemove));
+    }
+
+    public void play() {
+        Song songToPlay = this.songs.stream().findFirst().orElseThrow(() -> new RuntimeException(PLAYLIST_IS_EMPTY));
+        songToPlay.play();
+    }
+
+    public boolean isPlaying() {
+        return true;
+    }
+
+    public Song songPlaying() {
+        return songs
+                .stream()
+                .filter(Song::isPlaying)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(NO_SONG_PLAYING));
+    }
+
+    public Song songNamed(String songNameToLookFor) {
+        return songs
+                .stream()
+                .filter(song -> song.isNamed(songNameToLookFor))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(SONG_NOT_FOUND));
+    }
+
+    public void play(String songNameToPlay) {
+        songs.forEach(Song::pause);
+        songNamed(songNameToPlay).play();
+    }
+
+    void addSongs(String... songNames) {
+        for (String songName : songNames) {
+            addSong(songName);
+        }
     }
 }
--- server/src/test/java/io/franco/troubadour/streaming/PlaylistTest.java
+++ server/src/test/java/io/franco/troubadour/streaming/PlaylistTest.java
@@ -3,11 +3,13 @@ package io.franco.troubadour.streaming;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PlaylistTest {
 
     private static final String PLAYLIST_NAME = "a playlist";
     public static final String SONG_NAME = "a songName";
+    public static final String ANOTHER_SONG_NAME = "another songName";
 
     @Test
     public void isCreatedWithName() {
@@ -28,6 +30,51 @@ class PlaylistTest {
         playlist.addSong(SONG_NAME);
         playlist.removeSong(SONG_NAME);
         assertThat(playlist.hasSong(SONG_NAME)).isFalse();
+    }
+
+    @Test
+    public void canNotAddSameSongTwice() {
+        var playlist = Playlist.named(PLAYLIST_NAME);
+        playlist.addSong(SONG_NAME);
+
+        var ex = assertThrows(RuntimeException.class,
+                () -> playlist.addSong(SONG_NAME));
+
+        assertThat(ex.getMessage()).isEqualTo(Playlist.ALREADY_CONTAINS_SONG);
+    }
+
+    @Test
+    public void canPlaySongsFromTheBeginningOfPlaylist() {
+        var playlist = Playlist.named(PLAYLIST_NAME);
+        playlist.addSongs("another songName", SONG_NAME);
+
+        playlist.play();
+
+        assertThat(playlist.isPlaying()).isTrue();
+        assertThat(playlist.songPlaying()).isEqualTo(playlist.songNamed(SONG_NAME));
+    }
+
+    @Test
+    public void canPlaySongsFromAnywhereInThePlaylist() {
+        var playlist = Playlist.named(PLAYLIST_NAME);
+        playlist.addSongs(ANOTHER_SONG_NAME, SONG_NAME);
+
+        playlist.play(ANOTHER_SONG_NAME);
+
+        assertThat(playlist.isPlaying()).isTrue();
+        assertThat(playlist.songPlaying()).isEqualTo(playlist.songNamed(ANOTHER_SONG_NAME));
+    }
+
+    @Test
+    public void GivenPlayCommand_WhenSongIsPlaying_ItSwitchesToNewSong() {
+        var playlist = Playlist.named(PLAYLIST_NAME);
+        playlist.addSongs(ANOTHER_SONG_NAME, SONG_NAME);
+        playlist.play(ANOTHER_SONG_NAME);
+
+        playlist.play(SONG_NAME);
+
+        assertThat(playlist.songPlaying()).isEqualTo(playlist.songNamed(SONG_NAME));
+        assertThat(playlist.songNamed(ANOTHER_SONG_NAME).isPlaying()).isFalse();
     }
 
 }
